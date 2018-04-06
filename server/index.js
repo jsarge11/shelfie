af07f21f69193f9da00564b70e6b10d69face573
Jay Sargent <sargentjay11@gmail.com>
2018-04-06 11:15:43 -0600

    database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const axios = require('axios')
 const express = require('express')
-const bodyParser = require('body-parser') 
+const bodyParser = require('body-parser')
+const massive = require('massive')
+require('dotenv').config();
 
 let app = express();
 app.use(bodyParser.json());
 
-const port = 4000;
-app.listen(port, ()=>console.log(`The Daelaam wait for your action on port ${port}, executor.`))
+const connectionString = 'postgres://khqgmebzfsmfqt:f283f804a31f7789e86c54da5fa20048d276eefb0a70dda07547f59104162666@ec2-54-204-21-226.compute-1.amazonaws.com:5432/dbcliuko1mdqn1?ssl=true';
+
+massive(connectionString).then(connection => {
+ app.set('db', connection); //hold onto this thought // no correlation between this and the db folder // 
+ let port = 4000;
+ app.listen(port, ()=>console.log(`The Daelaam wait for your action on port ${port}, executor.`))
+});
+
+
