c1d93ac8fee10563193fb74a787d3f7a58a3e344
Jay Sargent <sargentjay11@gmail.com>
2018-04-06 11:36:44 -0600

    part 1 step 1

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -6,6 +6,7 @@ export default class Dashboard extends Component {
  render() {
   return( 
    <div>
+
     Dashboard
     <Product />
    </div>
--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -2,10 +2,54 @@ import React, {Component} from 'react'
 
 
 export default class Form extends Component {
+ constructor() {
+  super() 
+
+  this.state = {
+   product_name : '',
+   product_price : 0.00,
+   user_image_url: '',
+   image_url : 'https://www.endurasport.com/assets/images/prodbrowser/product_placeholder.jpg'
+  }
+ }
+ updateName(text) {
+  this.setState({
+   product_name: text
+  })
+ }
+ updatePrice(num) {
+  this.setState({
+   product_price: num
+  })
+ }
+ updateURL(text) {
+  this.setState({
+   user_image_url: text
+  })
+ }
+ resetInput() {
+  this.setState({
+   product_name : '',
+   product_price : 0.00,
+   user_image_url : ''
+  })
+ }
+ 
  render() {
   return( 
-   <div>
-    Form
+   <div className="formWrapper">
+    <div>
+     <img src={this.state.image_url} alt='exampleimg'/>
+    </div>
+    <div className="inputWrapper">
+     <input onChange={(e)=>this.updateURL(e.target.value)} type='text' placeholder="image url goes here ... " value={this.state.user_image_url}/>
+     <input onChange={(e)=>this.updateName(e.target.value)} type='text' placeholder="product name goes here ... " value={this.state.product_name}/>
+     <input onChange={(e)=>this.updatePrice(e.target.value)} type='text'placeholder="price goes here ... " value={this.state.product_price}/>
+    </div>
+    <div buttonWrapper>
+     <button onClick={()=>this.resetInput()}>Cancel</button>
+     <button>Add to Inventory</button>
+    </div>
    </div>
   )
  }
